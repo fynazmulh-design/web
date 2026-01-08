395aa4084874bbe1690fdaa771351773aea72214
fynazmulh-design <fynazmulh@gmail.com>
2026-01-08 06:09:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -201,7 +201,7 @@ function updateUIBasedOnPlan(plan) {
             dashBtn.onclick = function() { window.open(guideLink, '_blank'); };
             dashBtn.querySelector('i').className = "ph-fill ph-play-circle text-2xl";
             if(dashText) dashText.innerText = "Watch Tutorial / Guide";
-        }
+        
         
         // Reset Public Button
         if(publicBtn) {
@@ -368,6 +368,7 @@ function handleAuth(event, action) { event.preventDefault(); const form = event.
 function logout() { localStorage.removeItem('proToolsUser'); location.reload(); }
 function togglePassword(inputId, icon) { const input = document.getElementById(inputId); if (input.type === "password") { input.type = "text"; icon.classList.remove('ph-eye'); icon.classList.add('ph-eye-slash'); } else { input.type = "password"; icon.classList.remove('ph-eye-slash'); icon.classList.add('ph-eye'); } }
 function checkAccess(toolId) { const user = JSON.parse(localStorage.getItem('proToolsUser')); if (!user || !user.isLoggedIn) { alert("Please login first!"); return; } if (!user.plan || user.plan === 'Free') { document.getElementById('lockModal').classList.remove('hidden'); } else { loadTool(toolId); } }
+
 
 
 
