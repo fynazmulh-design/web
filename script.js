a5337ddaae894c9b1999467c02d766a4ac356557
fynazmulh-design <fynazmulh@gmail.com>
2026-01-08 11:28:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -79,14 +79,6 @@ const commonNumbers = ["123", "007", "101", "1", "99", "88", "55", "777", "24", 
 let isLoggedIn = false;
 let currentTool = null;
 
-// SECURITY: BLOCK RIGHT CLICK & INSPECT
-document.addEventListener('contextmenu', event => event.preventDefault());
-document.onkeydown = function(e) {
-    if(e.keyCode == 123) return false;
-    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) return false;
-    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) return false;
-    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) return false;
-}
 
 window.onload = function() {
     setupSite();
@@ -391,6 +383,7 @@ function updatePayInfo() {
         senderInput.placeholder = "01xxxxxxxxx";
     }
 }
+
 
 
 
