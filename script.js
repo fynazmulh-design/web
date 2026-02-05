ef9f8f30654399536afba4a59d4d90706ffa25f6
fynazmulh-design <fynazmulh@gmail.com>
2026-02-05 10:23:23 +0600

    Update script.js

--- script.js
+++ script.js
@@ -293,7 +293,7 @@ function loadTool(toolId) {
     output.innerHTML = ""; controls.innerHTML = "";
 
     // 🔗 LINK TOOLS
-    if (['video', 'cpa', 'ip', 'software'].includes(toolId)) {
+    if (['cpa', 'ip', 'software'].includes(toolId)) {
         let toolName = toolId === 'cpa' ? 'CPA Networks' : (toolId === 'ip' ? 'IP Lists' : 'Course Video');
         let icon = toolId === 'cpa' ? 'ph-globe' : (toolId === 'ip' ? 'ph-globe-ip' : 'ph-hard-drives');
         title.innerHTML = `<i class="ph-fill ${icon} text-purple-400"></i> ${toolName}`;
@@ -386,6 +386,7 @@ function updatePayInfo() {
         senderInput.placeholder = "01xxxxxxxxx";
     }
 }
+
 
 
 
