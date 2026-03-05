d23297bcc3730424ee1441e57c57f9f18e371900
Freelancer Nazmul <fynazmulh@gmail.com>
2026-03-05 14:19:20 +0600

    Update script.js

--- script.js
+++ script.js
@@ -59,6 +59,7 @@ const LINK_DATA = {
     ],
     address: [
         { name: "All Country Address Site", url: "https://www.ssa.gov/locator/results?q=new%20york%201001" },
+    ],   
     name: [
         { name: "Contact Support Only Telegram (সাপোর্ট সপ্তাহে 6 দিন ! দুপুর 2:00 টা থেকে রাত 9:00 টা পর্যন্ত ! শুক্রবারে সাপোর্ট সম্পূর্ণ বন্ধ) 👩‍💻👉", url: "https://t.me/studentssupportfn" },
     ]
@@ -356,6 +357,7 @@ function updatePayInfo() {
         senderInput.placeholder = "01xxxxxxxxx";
     }
 }
+
 
 
 
